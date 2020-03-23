01e9d78d767abb12d500724e68fd94bcced1404b
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-03-23 17:07:28 +0100

    Update counter.py

--- counter.py
+++ counter.py
@@ -10,6 +10,9 @@ counter = Button(23,bounce_time=0.05)
 def counter_action():
     response = urlopen('https://127.0.0.1/json.htm?type=command&param=udevice&idx=50&svalue=1', context=ssl._create_unverified_context())
 
+#We can count every occurence when button was pressed.    
 counter.when_pressed = counter_action
+#Or we can count every occurence when button is pressed longer than bounce_time.
+#counter.when_held = counter_action
 
 pause()
