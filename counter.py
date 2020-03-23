bbafb3356362495aadb4b8af8b08b93350700aac
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-03-23 17:11:42 +0100

    Update counter.py

--- counter.py
+++ counter.py
@@ -6,10 +6,11 @@ from urllib.request import urlopen
 import ssl
 
 counter = Button(23,bounce_time=0.05)
+idx_value = 50
 
 def counter_action():
-    response = urlopen('https://127.0.0.1/json.htm?type=command&param=udevice&idx=50&svalue=1', context=ssl._create_unverified_context())
-
+    #response = urlopen('https://127.0.0.1/json.htm?type=command&param=udevice&idx=50&svalue=1', context=ssl._create_unverified_context())
+    response = urlopen('https://127.0.0.1/json.htm?type=command&param=udevice&idx=' + str(idx_value) + '&svalue=1', context=ssl._create_unverified_context())
 #We can count every occurence when button was pressed.    
 counter.when_pressed = counter_action
 
