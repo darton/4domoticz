7e7d28df5c025b19825823b4bf09786f31ce46bf
Dariusz Kowalczyk <darton@users.noreply.github.com>
2020-03-23 16:49:09 +0100

    Update counter.py

--- counter.py
+++ counter.py
@@ -5,11 +5,11 @@ from signal import pause
 from urllib.request import urlopen
 import ssl
 
-button = Button(23,bounce_time=0.05)
+counter = Button(23,bounce_time=0.05)
 
-def button_action():
+def counter_action():
     response = urlopen('https://127.0.0.1/json.htm?type=command&param=udevice&idx=50&svalue=1', context=ssl._create_unverified_context())
 
-button.when_pressed = button_action
+counter.when_pressed = counter_action
 
 pause()
